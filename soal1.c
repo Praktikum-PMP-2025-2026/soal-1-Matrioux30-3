6c57bb3c6680d98e0241c68cdbe60aff684ccc85
Matrioux30 <izhharulmulki@gmail.com>
2026-04-29 10:13:28 +0700

    .
    .

--- soal1.c
+++ soal1.c
@@ -1,5 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
+typedef struct{
+    char nama[30];
+    char kategori[30];
+    int tahun;
+    int nilai;
+}artefak;
+
+void swap(artefak *a, artefak *b){
+    artefak temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void best(artefak *data, int N){
+    for(int j = 0; j<N-1;j++){
+        for(int i=0; i<N-j-1;i++){
+            if (strcmp(data[i].kategori, data[i+1].kategori)>0) {
+                swap(&data[i],&data[i+1]);
+            }
+            else if(data[i].kategori == data[i+1].kategori){
+                if(data[i].tahun > data[i+1].tahun){
+                    swap(&data[i],&data[i+1]);
+            }
+                else if(data[i].tahun == data[i+1].tahun){
+                    if(data[i].nilai < data[i+1].nilai){
+                        swap(&data[i],&data[i+1]);
+            }
+                    else if(data[i].nilai == data[i+1].nilai){
+                        if(strcmp(data[i].nama, data[i+1].nama) > 0){
+                            swap(&data[i],&data[i+1]);
+            }
+            }
+            }
+            }
+        }
+    }
+}
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    artefak kategori[N];
+
+    for(int i=0; i<N; i++){
+        scanf("%s %s %d %d", &kategori[i].nama, &kategori[i].kategori, &kategori[i].tahun, &kategori[i].nilai);
+    }
+
+    best(kategori, N);
+
+    for(int i=0; i<N; i++){
+        printf("%s %s %d %d\n", kategori[i].nama, kategori[i].kategori, kategori[i].tahun, kategori[i].nilai);
+    }
     return 0;
 }
